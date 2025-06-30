639f9a87c7ad8a804ba2c78f81fc80a2ae0f8b8d
ictorv <victor.mygit@gmail.com>
2025-06-30 18:02:00 +0530

    feat: Added Flow 1

--- src/test/java/linearFlow/Flow_1.java
+++ src/test/java/linearFlow/Flow_1.java
@@ -2,16 +2,17 @@
 
 package linearFlow;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 public class Flow_1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://www.coursera.org/");
 		driver.manage().window().maximize();
@@ -20,11 +21,57 @@ public class Flow_1 {
 		search.sendKeys("Web Development");
 		search.sendKeys(Keys.ENTER);
 		
+		//For More Languages
+		WebElement allLang=driver.findElement(By.cssSelector("button[aria-label='Show more Language options'] span[class='cds-button-label']"));
+		allLang.click();
+		
+		//For ALl languages in list
+		List <WebElement> lang=driver.findElements(By.xpath("//*[@class=\"css-q5d1os\"]/div"));
+		for (WebElement ele: lang) {
+			if(ele.getText().contains("English")) {
+				ele.click();
+			}
+			
+		}
 //		driver.findElement(By.xpath("//*[contains(@magnifier-wrapper, 'Submit Search' )][1]")).click();
 //		driver.findElement(By.xpath("//*[@id=\"rendered-content\"]/div/div/span/div[1]/header/div[1]/div/div[2]/div/div/div/div/div/div[3]/div/div[2]/form/div/button[2]/div")).click();	
-		Actions act= new Actions(driver);
-		act.keyDown(Keys.ENTER);
+//		Actions act= new Actions(driver);
+//		act.keyDown(Keys.ENTER);
 		
+		//Select Level
+		List <WebElement> lvl=driver.findElements(By.xpath("//div[@data-testid='search-filter-group-Level']//div/span"));
+		for(WebElement ele:lvl) {
+			if(ele.getText().contains("Beginner")) {
+				ele.click();
+			}
+		}
+
+		//wait for loading element
+		WaitUtils.waitForDuration(driver, 5);
+		
+		//Get card
+		List <WebElement> cards=driver.findElements(By.xpath("//*[@class='cds-ProductCard-gridCard']"));
+		
+		//for each card
+		for(WebElement card:cards) {
+			//get title for card
+			WebElement tit=card.findElement(By.xpath(".//h3"));  ///getting title
+			System.out.println(tit.getText());
+
+			WebElement rating = card.findElement(By.xpath(".//*[@class='css-6ecy9b']")); //getting rating
+			System.out.println(rating.getText());
+		
+
+			WebElement duration = card.findElement(By.xpath(".//*[@class='cds-CommonCard-metadata']"));  //getting duration
+			String dur=duration.getText();
+			String[] parts = dur.split("·");
+			
+			// Trim and get the last part
+			String time = parts[parts.length - 1].trim();
+
+			System.out.println(time);
+			
+		}
 	}
 
 }
