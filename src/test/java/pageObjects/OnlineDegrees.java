76aa9d5942b0d87b6acb1c769db328d992452381
Arpan Basak <arpanbasak.official@gmail.com>
2025-07-02 16:06:08 +0530

    Feat 02/07/25 04.06 PM TC_005

--- src/test/java/pageObjects/OnlineDegrees.java
+++ src/test/java/pageObjects/OnlineDegrees.java
@@ -8,88 +8,74 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class OnlineDegrees extends BasePage {
-	
-	public OnlineDegrees(WebDriver driver) {
-		super(driver);
-		
-	}
 
-	@FindBy(xpath="//button[@id='react-aria-:R159l6kala:']//span[@class='cds-selectController-icon']//*[name()='svg']")
-	WebElement programLevel ;
-	
-	@FindBy(xpath="//div[@role='presentation' and @dir='ltr']//li/div/div/div/div[1]")
-	List<WebElement> progamLevelList;
-	
-	@FindBy(xpath="//span[normalize-space()='Apply']")
-	WebElement applyBtn;
-	
-	@FindBy(xpath="//button[@id='react-aria-:R179l6kala:']//span[@class='cds-selectController-icon']//*[name()='svg']")
-	WebElement subject;
-	
-	@FindBy(xpath="//div[@class='cds-popoverContainer-inner']//li/div/div/div/div[1]")
-	List<WebElement> subjectList;
-	
-	@FindBy(xpath="//div[@id='DegreeFilters']/div[2]/div[1]//span")
-	WebElement noOfResults ;
-	
-	@FindBy(xpath="//div[@data-test=\"TopProductsList\"]/div/div")
-	List<WebElement> collegeCards;
-	
-	public void dropdownProgramLevel()
-	{
-		programLevel.click();
-	}
-	
-	public void selectProgramLevel(String value)
-	{
-		for(WebElement ele : progamLevelList)
-		{
-			System.out.println(ele.getText());
-			if(ele.getText().contains(value))
-			{
-				ele.click();
-			}
-		}
-	}
-	
-	public void btnApply()
-	{
-		applyBtn.click();
-	}
-	
-	public void dropdownSubject()
-	{
-		subject.click();
-	}
-	
-	public void selectSubject(String value)
-	{
-		for(WebElement ele : subjectList)
-		{
-			System.out.println(ele.getText());
-			if(ele.getText().contains(value))
-			{
-				ele.click();
-			}
-		}
-	}
-	// Again apply click method
-	
-	public String displayNoOfResults()
-	{
-		return noOfResults.getText();
-	}
-	
-	public List<String> displayCollegeDetails() 
-	{
-	    List<String> details = new ArrayList<>();
-	    for (WebElement ele : collegeCards) 
-	    {
-	        details.add(ele.getText());
-	    }
-	    return details;
-	}
+    public OnlineDegrees(WebDriver driver) {
+        super(driver);
+    }
 
-	
+    //  Filters
+    @FindBy(xpath = "//button[@id='react-aria-:R159l6kala:']")
+    WebElement programLevelDropdown;
 
+    @FindBy(xpath = "//div[@role='presentation' and @dir='ltr']//li/div/div/div/div[1]")
+    List<WebElement> programLevelOptions;
+
+    @FindBy(xpath = "//button[@id='react-aria-:R179l6kala:']")
+    WebElement subjectDropdown;
+
+    @FindBy(xpath = "//div[@class='cds-popoverContainer-inner']//li/div/div/div/div[1]")
+    List<WebElement> subjectOptions;
+
+    @FindBy(xpath = "//span[normalize-space()='Apply']")
+    WebElement applyButton;
+
+    @FindBy(xpath = "//div[@id='DegreeFilters']/div[2]/div[1]//span")
+    WebElement totalResultsText;
+
+    @FindBy(xpath = "//div[@data-test='TopProductsList']/div/div")
+    List<WebElement> collegeCards;
+
+    // Program Level Methods
+    public void openProgramLevelDropdown() {
+        scrollIntoView(programLevelDropdown);
+        programLevelDropdown.click();
+    }
+
+    public void selectProgramLevelByIndex(int index) {
+        if (index >= 0 && index < programLevelOptions.size()) {
+            programLevelOptions.get(index).click();
+        }
+    }
+
+
+    //  Subject Methods
+    public void openSubjectDropdown() {
+        scrollIntoView(subjectDropdown);
+        subjectDropdown.click();
+    }
+
+    public void selectSubjectByIndex(int index) {
+        if (index >= 0 && index < subjectOptions.size()) {
+            subjectOptions.get(index).click();
+        }
+    }
+
+    //  Apply Filters
+    public void applyFilters() {
+        scrollIntoView(applyButton);
+        applyButton.click();
+    }
+
+    //  Results Info
+    public String getTotalResults() {
+        return totalResultsText.getText();
+    }
+
+    public List<String> getCollegeDetails() {
+        List<String> details = new ArrayList<>();
+        for (WebElement card : collegeCards) {
+            details.add(card.getText());
+        }
+        return details;
+    }
 }
