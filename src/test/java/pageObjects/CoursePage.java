fe66185b311d81d29677dd5e553325365121ccbd
ictorv <victor.mygit@gmail.com>
2025-07-02 12:25:29 +0530

    feat: Added Course Page and Test

---
+++ src/test/java/pageObjects/CoursePage.java
@@ -0,0 +1,106 @@
+package pageObjects;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CoursePage extends BasePage{
+	public CoursePage(WebDriver driver) {
+		super(driver);
+	}
+	
+	//Expanding Language
+	@FindBy(css ="button[aria-label='Show more Language options'] span[class='cds-button-label']")
+	WebElement allLang ;
+	
+	//List of Languages
+	@FindBy(xpath="//*[@class='css-q5d1os']/div")
+	List <WebElement> lang ;
+
+	//List of Levels
+	@FindBy(xpath="//div[@data-testid='search-filter-group-Level']//div/span")
+	List <WebElement> level ;
+
+	//List of Courses
+	@FindBy(xpath="//*[@class='cds-ProductCard-gridCard']")
+	List<WebElement> cards ;
+	
+	Map<String, Integer> languageMap;
+	
+	//Getting all languages in HashMap
+	public void getLanguages() {
+		languageMap = new HashMap<>();
+		Pattern pattern = Pattern.compile("^(.*)\\((\\d+)(?:,(\\d+))?\\)$");
+		for(WebElement ele: lang) {
+			String tex= ele.getText();
+			Matcher matcher = pattern.matcher(tex.trim());//got 3 group
+			
+			if (matcher.find()) {
+				String name = matcher.group(1).trim();  //group 1
+				String number = matcher.group(3) != null ? matcher.group(2)+matcher.group(3) : matcher.group(2);  //group 2 & 3 for numbers
+				languageMap.put(name, Integer.parseInt(number));	
+			}
+		}
+//		return languageMap;
+	}
+	
+	//Clicking on All Language Expansion
+	public void clickallLang()
+	{
+		allLang.click();
+	}
+
+	//Selecting Language
+	public void selectLang(String value)
+	{
+		for (WebElement ele: lang) 
+		{
+			if(ele.getText().contains(value)) 
+			{
+				ele.click();
+			}
+		}
+	}
+		
+	//Selecting Level
+	public void setLevel(String value)
+	{
+		for(WebElement ele:level) 
+		{
+			if(ele.getText().contains(value)) 
+			{
+				ele.click();
+			}
+		}
+	}
+	
+	//get course card by index
+	public WebElement getCard(int index) {
+		return cards.get(index);
+	}
+	
+	//get title of specific course
+	public String getTitle(WebElement card) {
+		return card.findElement(By.xpath(".//h3")).getText();  
+	}
+	
+	//get rating of specific course
+	public String getRating(WebElement card) {
+		return card.findElement(By.xpath(".//*[@class='css-6ecy9b']")).getText();
+	}
+	
+	//get duration of specific course
+	public String getDuration(WebElement card) {
+		String dur = card.findElement(By.xpath(".//*[@class='cds-CommonCard-metadata']")).getText();  //getting duration
+		String[] parts = dur.split("·");
+				
+		return parts[parts.length - 1].trim(); // Trim and get the last part
+	}		
+}
--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -21,119 +21,74 @@ public class HomePage extends BasePage{
 
 	JavascriptExecutor js = (JavascriptExecutor) driver;
 	TakesScreenshot ts = (TakesScreenshot)driver;
+		
+	//Search Bar
+	@FindBy(id="search-autocomplete-input")
+	WebElement searchBar ;
 	
-//Search Bar
-@FindBy(id="search-autocomplete-input")
-WebElement searchBar ;
-
-//For Government
-@FindBy(xpath="//a[@data-click-key='front_page.front_page_story.click.navigation_meta_nav_Government']")
-WebElement forGovernment ;
-
-//Online Degrees
-@FindBy(xpath="//a[normalize-space()='Online Degrees']")
-WebElement onlineDegree;
-
-//Bottom IOS Store
-@FindBy(xpath="//a[@data-click-key='front_page.front_page_story.click.mobile_app_badges_ios']")
-WebElement appStore;
-
-//Bottom Play Store
-@FindBy(xpath="//a[@data-click-key='front_page.front_page_story.click.mobile_app_badges_android']")
-WebElement playStore;
-
-@FindBy(css ="button[aria-label='Show more Language options'] span[class='cds-button-label']")
-WebElement allLang ;
-@FindBy(xpath="//*[@class='css-q5d1os']/div")
-List <WebElement> lang ;
-
-@FindBy(xpath="//div[@data-testid='search-filter-group-Level']//div/span")
-List <WebElement> level ;
-
-@FindBy(xpath="//*[@class='cds-ProductCard-gridCard']")
-List<WebElement> cards ;
-
-public void clickForGovernment()
-{
-	forGovernment.click();
-}
-
-public void setSearchBar(String searchValue)
-{
-	searchBar.sendKeys(searchValue);
-}
-
-public void clickSearchBar()
-{
-	searchBar.sendKeys(Keys.ENTER);
-}
-
-
-
-public void clickOnlineDegree()
-{
-	onlineDegree.click();
-}
-
-
-//IOS Store Click
-public void clickAppStore()
-{
-	js.executeScript("arguments[0].scrollIntoView(false);", appStore);
-    appStore.click();
-}
-//TakeScrrenShot
-public void clickScreenShotIOS()
-{
-	File Sourcefile1 = ts.getScreenshotAs(OutputType.FILE);
-    File targetFile1 = new File(System.getProperty("user.dir")+"\\Validation_ScreenShot\\Apple.png");
-    Sourcefile1.renameTo(targetFile1);
-}
-//PlayStore Click
-public void clickPlayStore()
-{
-	playStore.click();
-}
-
-//TakeScreenShot
-public void clickScreenShotAndroid()
-{
-	File Sourcefile2 = ts.getScreenshotAs(OutputType.FILE);
-    File targetFile2 = new File(System.getProperty("user.dir")+"\\Validation_ScreenShot\\Android.png");
-    Sourcefile2.renameTo(targetFile2);
-}
-
-public void clickallLang()
-{
-	allLang.click();
-}
-
-public void selectLang(String value)
-{
-	for (WebElement ele: lang) 
+	//For Government
+	@FindBy(xpath="//a[@data-click-key='front_page.front_page_story.click.navigation_meta_nav_Government']")
+	WebElement forGovernment ;
+	
+	//Online Degrees
+	@FindBy(xpath="//a[normalize-space()='Online Degrees']")
+	WebElement onlineDegree;
+	
+	//Bottom IOS Store
+	@FindBy(xpath="//a[@data-click-key='front_page.front_page_story.click.mobile_app_badges_ios']")
+	WebElement appStore;
+	
+	//Bottom Play Store
+	@FindBy(xpath="//a[@data-click-key='front_page.front_page_story.click.mobile_app_badges_android']")
+	WebElement playStore;
+	
+	
+	public void clickForGovernment()
 	{
-		if(ele.getText().contains(value)) 
-		{
-			ele.click();
-		}
+		forGovernment.click();
 	}
-}
 	
-public void setLevel(String value)
-{
-	for(WebElement ele:level) 
+	public void setSearchBar(String searchValue)
 	{
-		if(ele.getText().contains(value)) 
-		{
-			ele.click();
-		}
+		searchBar.sendKeys(searchValue);
 	}
-}
 	
+	public void clickSearchBar()
+	{
+		searchBar.sendKeys(Keys.ENTER);
+	}
 	
 
+	public void clickOnlineDegree()
+	{
+		onlineDegree.click();
+	}
+	
 
-
-
-
+	//IOS Store Click
+	public void clickAppStore()
+	{
+		js.executeScript("arguments[0].scrollIntoView(false);", appStore);
+	    appStore.click();
+	}
+	//TakeScrrenShot
+	public void clickScreenShotIOS()
+	{
+		File Sourcefile1 = ts.getScreenshotAs(OutputType.FILE);
+	    File targetFile1 = new File(System.getProperty("user.dir")+"\\Validation_ScreenShot\\Apple.png");
+	    Sourcefile1.renameTo(targetFile1);
+	}
+	//PlayStore Click
+	public void clickPlayStore()
+	{
+		playStore.click();
+	}
+	
+	//TakeScreenShot
+	public void clickScreenShotAndroid()
+	{
+		File Sourcefile2 = ts.getScreenshotAs(OutputType.FILE);
+	    File targetFile2 = new File(System.getProperty("user.dir")+"\\Validation_ScreenShot\\Android.png");
+	    Sourcefile2.renameTo(targetFile2);
+	}
 }
