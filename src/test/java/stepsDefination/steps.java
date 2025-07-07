c2680be6c0faaf458ff5d9935f7df31a07810c9a
ictorv <victor.mygit@gmail.com>
2025-07-07 20:05:09 +0530

    feat: dependencies in pom and initial step

--- src/test/java/stepsDefination/steps.java
+++ src/test/java/stepsDefination/steps.java
@@ -1,98 +1,96 @@
 package stepsDefination;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-import factory.BaseClass;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.en.*;
+import pageObjects.CoursePage;
 import pageObjects.HomePage;
+import factory.BaseClass;
 
 public class steps {
-	WebDriver driver;
-	HomePage hp;
-	@Given("User is on homepage")
-	public void user_is_on_homepage() {
-		driver=BaseClass.getDriver();
-		hp = new HomePage(driver);
-	}
 
-	@When("search course in search bar")
-	public void search_course_in_search_bar() {
-		hp.setSearchBar("Web Development");
-		hp.submitSearch();
-		
-	}
+    WebDriver driver;
+    HomePage hp;
+    CoursePage cp;
+    WebElement card;
 
-	@Then("course details page opens")
-	public void course_details_page_opens() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @Given("User is on homepage")
+    public void user_is_on_homepage() throws Exception {
+        driver = BaseClass.initilizeBrowser();
+        hp = new HomePage(driver);
+    }
 
-	@Given("hidden languages in page need to click to show all")
-	public void hidden_languages_in_page_need_to_click_to_show_all() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @When("search course in search bar")
+    public void search_course_in_search_bar() {
+        hp.setSearchBar("Web Development");
+        hp.submitSearch();
+        cp = new CoursePage(driver);
+    }
 
-	@When("click on specific language")
-	public void click_on_specific_language() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @Then("course details page opens")
+    public void course_details_page_opens() {
+        System.out.println("Course details page opened.");
+    }
 
-	@Then("language get selected")
-	public void language_get_selected() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @Given("hidden languages in page need to click to show all")
+    public void hidden_languages_in_page_need_to_click_to_show_all() {
+        cp.clickallLang();
+    }
 
-	@Given("levels section")
-	public void levels_section() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @When("click on specific language")
+    public void click_on_specific_language() {
+        cp.selectLang("English");
+    }
 
-	@When("click on specific level")
-	public void click_on_specific_level() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @Then("language get selected")
+    public void language_get_selected() {
+        System.out.println("Language selected.");
+    }
 
-	@Then("level get selected")
-	public void level_get_selected() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @Given("levels section")
+    public void levels_section() {
+        // Already covered
+    }
 
-	@Given("cards of first course")
-	public void cards_of_first_course() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @When("click on specific level")
+    public void click_on_specific_level() {
+        cp.setLevel("Beginner");
+    }
 
-	@When("get title rating and duration")
-	public void get_title_rating_and_duration() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @Then("level get selected")
+    public void level_get_selected() {
+        System.out.println("Level selected.");
+    }
 
-	@Then("title rating and duration for first course are fetched")
-	public void title_rating_and_duration_for_first_course_are_fetched() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @Given("cards of first course")
+    public void cards_of_first_course() {
+        card = cp.getCard(0);
+    }
 
-	@Given("cards of second course")
-	public void cards_of_second_course() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @When("get title rating and duration")
+    public void get_title_rating_and_duration() {
+        String title = cp.getTitle(card);
+        String rating = cp.getRating(card);
+        String duration = cp.getDuration(card);
+        System.out.println(title + "__" + rating + "__" + duration);
+    }
 
-	@Then("title rating and duration for second course are fetched")
-	public void title_rating_and_duration_for_second_course_are_fetched() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+    @Then("title rating and duration for first course are fetched")
+    public void title_rating_and_duration_for_first_course_are_fetched() {
+        System.out.println("First course details fetched.");
+    }
+
+    @Given("cards of second course")
+    public void cards_of_second_course() {
+        card = cp.getCard(1);
+    }
+
+    @Then("title rating and duration for second")
+    public void title_rating_and_duration_for_second() {
+        String title = cp.getTitle(card);
+        String rating = cp.getRating(card);
+        String duration = cp.getDuration(card);
+        System.out.println(title + "__" + rating + "__" + duration);
+    }
 }
--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,5 +1,20 @@
 package testRunner;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
+@CucumberOptions(
+    features = "src/test/java/features",
+    glue = {"stepDefinitions"},
+    plugin = {"pretty", "html:target/cucumber-reports"},
+    monochrome = true
+)
 public class TestRunner {
 
 }
+
+
+
+
+//
+//public class TestRunner extends AbstractTestNGCucumberTests {
+//}
